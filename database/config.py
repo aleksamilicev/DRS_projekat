8f33df3b60ca5a99b20dc195783e411acb300819
aleksamilicev <amilicev92@gmail.com>
2024-12-05 15:28:47 +0100

    samo sam promenio user i pass da bi koristio bazu

--- database/config.py
+++ database/config.py
@@ -2,7 +2,7 @@ class Config:
     # Proper connection string with service name
     SQLALCHEMY_DATABASE_URI = (
         #"oracle+cx_oracle://Username:Password@localhost:1521/?service_name=ServiceName)"
-        "oracle+cx_oracle://Slavko:ftn@localhost:1521/?service_name=xepdb1"
+        "oracle+cx_oracle://skalarr:sifra123@localhost:1521/?service_name=xepdb1"
     )  
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Suppress warnings
     SQLALCHEMY_ECHO = False  # Optional: True to log SQL queries
