da2b7df607b170a10fdf85d9dabf009ca383aa49
aleksamilicev <amilicev92@gmail.com>
2025-07-08 22:11:55 +0200

    username i pass od sql-a

--- database/config.py
+++ database/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "oracle+cx_oracle://slavko:ftn@host.docker.internal:1521/?service_name=xepdb1")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "oracle+cx_oracle://skalarr:sifra123@host.docker.internal:1521/?service_name=xepdb1")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
