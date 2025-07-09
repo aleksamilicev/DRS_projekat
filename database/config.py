00ff693baa44fe285cf4125adc7f41c563737f13
Slavkoo01 <134085710+Slavkoo01@users.noreply.github.com>
2025-07-09 09:53:12 +0200

    Update config.py

--- database/config.py
+++ database/config.py
@@ -1,6 +1,10 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "oracle+cx_oracle://slavko:ftn@host.docker.internal:1521/?service_name=xepdb1")
+    # Format for connecting with SID instead of service_name
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL", 
+        "oracle+cx_oracle://admin:065235805Slavko@database-drs.cx8sygmoo3j3.eu-central-1.rds.amazonaws.com:1521/DATABASE"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
